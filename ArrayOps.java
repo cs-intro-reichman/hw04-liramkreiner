b46228c4ada00021ade1ec11178d690af3696f52
liram kreiner <liram.kreiner@post.runi.ac.il>
2024-01-08 13:29:45 +0200

    edit my oopsint

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,12 +1,7 @@
 public class ArrayOps {
     public static void main(String[] args)
     {
-        
-         System.out.println(findMissingInt(args[0]));
-         System.out.println(secondMaxValue(args[0]));
-         System.out.println(containsTheSameElements(args[0],args[1]));
-         System.out.println(isSorted(args[0]));
-
+       
     }
     
     public static int findMissingInt (int [] array) {
